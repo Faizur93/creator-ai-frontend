1f613a076c1ad1d1516b303bb78b330b73bd1044
Faizur93 <120652201+Faizur93@users.noreply.github.com>
2025-10-29 14:19:11 +0530

    Add initial Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { unoptimized: true } // simple for demo; replace with proper loader in prod
+}
+module.exports = nextConfig
